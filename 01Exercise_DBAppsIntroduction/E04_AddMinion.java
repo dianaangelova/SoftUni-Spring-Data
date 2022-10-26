44b76149f7e7bb83304b5fc6ab63465a568a81c3
Diana Angelova <diana.anguelova@gmail.com>
2022-10-26 20:06:52 +0300

    Update E04_AddMinion.java

--- 01Exercise_DBAppsIntroduction/E04_AddMinion.java
+++ 01Exercise_DBAppsIntroduction/E04_AddMinion.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class E04_AddMinion {
@@ -13,9 +10,11 @@ public class E04_AddMinion {
 
     private static final String INSERT_VILLAIN_SQL = "insert into villains (name, evilness_factor) values(?, \"evil\")";
 
-    private static final String GET_MINION_TOWN_SQL = "select towns.id as minion_town from towns where towns.name=?";
+    private static final String GET_MINION_TOWN_SQL = "select towns.id as town_id from towns where towns.name=?";
 
     private static final String INSERT_MINION_SQL = "insert into minions (name, age, town_id) values (?, ?, ?)";
+
+    private static final String INSERT_MINION_VILLAIN_SQL = "insert into minions_villains (minion_id, villain_id) values (?, ?)";
 
     public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
@@ -36,19 +35,26 @@ public class E04_AddMinion {
         PreparedStatement preparedStatementMinionTown = connection.prepareStatement(GET_MINION_TOWN_SQL);
         preparedStatementMinionTown.setString(1, minionTownInput);
 
-        ResultSet resultMinionTown = preparedStatementMinionTown.executeQuery();
+        ResultSet resultMinionTownID = preparedStatementMinionTown.executeQuery();
 
-        if (!resultMinionTown.next()) {
+        int generatedTownIDKey = 0;
 
-            PreparedStatement preparedStatementInsertTown = connection.prepareStatement(INSERT_TOWN_SQL);
+        if (!resultMinionTownID.next()) {
+
+            PreparedStatement preparedStatementInsertTown = connection.prepareStatement(INSERT_TOWN_SQL, Statement.RETURN_GENERATED_KEYS);
 
             preparedStatementInsertTown.setString(1, minionTownInput);
 
-            boolean resultTown = preparedStatementInsertTown.execute();
+            preparedStatementInsertTown.execute();
 
-            // if (preparedStatementInsertTown.execute()) {
+            ResultSet resultSetTownID = preparedStatementInsertTown.getGeneratedKeys();
+
+            if (resultSetTownID.next()) {
+                generatedTownIDKey = resultSetTownID.getInt(1);
+            }
+            if (generatedTownIDKey != 0) {
                 System.out.printf("Town %s was added to the database.%n", minionTownInput);
-            // }
+            }
         }
 
         PreparedStatement preparedStatementVillain = connection.prepareStatement(GET_VILLAIN_NAME_SQL);
@@ -56,25 +62,49 @@ public class E04_AddMinion {
 
         ResultSet resultVillain = preparedStatementVillain.executeQuery();
 
+        int generatedVillainIDKey = 0;
+
         if (!resultVillain.next()) {
-            PreparedStatement preparedStatementInsertVillain = connection.prepareStatement(INSERT_VILLAIN_SQL);
+            PreparedStatement preparedStatementInsertVillain = connection.prepareStatement(INSERT_VILLAIN_SQL, Statement.RETURN_GENERATED_KEYS);
 
-            preparedStatementInsertVillain.setString(1, minionTownInput);
+            preparedStatementInsertVillain.setString(1, villainInput);
 
-            boolean resultSQLVillain = preparedStatementInsertVillain.execute();
+            preparedStatementInsertVillain.execute();
 
-          // if (resultSQLVillain) {
+            ResultSet resultSetVillainID = preparedStatementInsertVillain.getGeneratedKeys();
+
+            if (resultSetVillainID.next()) {
+                generatedVillainIDKey = resultSetVillainID.getInt(1);
+            }
+
+            if (generatedVillainIDKey != 0) {
                 System.out.printf("Villain %s was added to the database.%n", villainInput);
-           // }
+            }
         }
 
-        PreparedStatement preparedStatementMinion = connection.prepareStatement(INSERT_MINION_SQL);
-        preparedStatementMinion.setString(1, minionNameInput);
-        preparedStatementMinion.setInt(2, minionAgeInput);
+        PreparedStatement preparedStatementInsertMinion = connection.prepareStatement(INSERT_MINION_SQL, Statement.RETURN_GENERATED_KEYS);
+        preparedStatementInsertMinion.setString(1, minionNameInput);
+        preparedStatementInsertMinion.setInt(2, minionAgeInput);
+        preparedStatementInsertMinion.setInt(3, generatedTownIDKey);
 
-        ResultSet resultMinionTownID = preparedStatementMinionTown.executeQuery();
-        preparedStatementMinion.setInt(3, resultMinionTownID.getInt(1));
+        preparedStatementInsertMinion.executeUpdate();
 
-        preparedStatementVillain.execute();
+        ResultSet resultMinionID = preparedStatementInsertMinion.getGeneratedKeys();
+
+        int generatedMinionIDKey = 0;
+
+        if (resultMinionID.next()) {
+            generatedMinionIDKey = resultMinionID.getInt(1);
+        }
+
+
+        PreparedStatement preparedStatementInsertMinVillaRelationship = connection.prepareStatement(INSERT_MINION_VILLAIN_SQL);
+        preparedStatementInsertMinVillaRelationship.setInt(1, generatedMinionIDKey);
+        preparedStatementInsertMinVillaRelationship.setInt(2, generatedVillainIDKey);
+
+        preparedStatementInsertMinVillaRelationship.execute();
+
+        System.out.printf("Successfully added %s to be minion of %s.", minionNameInput, villainInput);
+
     }
 }
