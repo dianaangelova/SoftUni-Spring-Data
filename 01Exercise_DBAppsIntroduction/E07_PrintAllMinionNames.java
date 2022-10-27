85698eaac81c2029689b6b494c39ee550314a680
Diana Angelova <diana.anguelova@gmail.com>
2022-10-27 15:54:12 +0300

    Update E07_PrintAllMinionNames.java

--- 01Exercise_DBAppsIntroduction/E07_PrintAllMinionNames.java
+++ 01Exercise_DBAppsIntroduction/E07_PrintAllMinionNames.java
@@ -3,7 +3,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayDeque;
-import java.util.Scanner;
 
 public class E07_PrintAllMinionNames {
 
