0e16fc931ae33388fab7eb5f73f622f9977a5c63
Diana Angelova <diana.anguelova@gmail.com>
2022-10-31 20:26:07 +0200

    Update MyConnector.java

--- 02Lab_ORMFundamentals/src/main/java/orm/MyConnector.java
+++ 02Lab_ORMFundamentals/src/main/java/orm/MyConnector.java
@@ -9,14 +9,16 @@ public class MyConnector {
 
     private static Connection connection;
 
-    private static final String connectionString = "jdbc:mysql://localhost:3306/%s";
+    private static String PARAMETERS_FOR_DATABASE = "?allowPublicKeyRetrieval=true&useSSL=false&createDatabaseIfNotExist=true&serverTimezone=UTC";
+
+    private static final String connectionString = "jdbc:mysql://localhost:3306/%s%s";
 
     public static void createConnection (String user, String password, String dbName) throws SQLException {
         Properties properties = new Properties();
         properties.setProperty("user", user);
         properties.setProperty("password", password);
 
-        String formattedJDBC = String.format(connectionString, dbName);
+        String formattedJDBC = String.format(connectionString, dbName, PARAMETERS_FOR_DATABASE);
 
         connection = DriverManager.getConnection(formattedJDBC, properties);
 
